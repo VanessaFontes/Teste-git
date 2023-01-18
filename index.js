a52f28df0756cde2a2f2252a0f93d281a9c4c2a7
VanessaFontes <vanessa-fontes.s@hotmail.com>
2023-01-18 19:26:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//declaração de variaveis
+
+let nota1 = 5;
+let nota2 = 10;
+
+//processamento
+let soma = nota1 + nota2
+let media = soma / 2;
+
+//exibição do resultado
+console.log("media do aluna: ",media);
+
